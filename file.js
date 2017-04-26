8b4b2035b02ae1dbf51c1a94d1a2198deef79ef2
Bruno Nigro <brunosopmac@gmail.com>
2017-04-26 18:59:10 -0300

    Concluding Chapter 1 close #1 close #2 close #3

---
+++ file.js
@@ -0,0 +1,7 @@
+function template(locals) {
+  var pug_html = "",
+    pug_mixins = {},
+    pug_interp;
+  pug_html = pug_html + "\u003Ch1\u003ESome pug\u003C\u002Fh1\u003E";;
+  return pug_html;
+}
